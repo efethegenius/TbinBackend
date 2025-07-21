7cccf2d8bd18963a019cb8bf102ffa3cdf8f310d
Efe Samuel <Efe.Samuel@coronationinsurance.com.ng>
2025-07-21 11:05:58 +0100

    allowed

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ const corsOptions = {
           origin === allowedOrigin ||
           origin.startsWith(allowedOrigin.replace("https://", "https://")) ||
           origin.endsWith(".netlify.app") || // Allow all Netlify preview deployments
-          origin.endsWith(".api.io") // Allow all Netlify preview deployments
+          origin.endsWith("api.io") // Allow all Netlify preview deployments
       )
     ) {
       return callback(null, true);
